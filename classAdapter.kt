ebb76de5bfe5182b7245e8d35bfc2c71c978fbef
Dhandy Joenathan <dhanjoenkp@gmail.com>
2021-09-28 19:46:58 +0700

    Update classAdapter.kt

--- classAdapter.kt
+++ classAdapter.kt
@@ -1,5 +1,11 @@
 class namaClass (val data: ArrayList<modelClass>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private class MyViewHolder(val binding: namaLayoutRecyclerView): RecyclerView.ViewHolder(binding.root)
+    
+    // code clickListener
+    private lateinit var onItemClickCallback: OnItemClickCallback
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return MyViewHolder(namaLayoutRecyclerView.inflate(LayoutInflater.from(parent.context), parent, false))
@@ -13,23 +19,16 @@ class namaClass (val data: ArrayList<modelClass>): RecyclerView.Adapter<Recycler
             holder.binding.tvJudul.text = model.judul
 
             holder.itemView.setOnClickListener {
-                onClickListener!!.onClick(position, model)
+                onItemClickCallback.onItemClicked(data[position])
             }
         }
     }
 
-    override fun getItemCount(): Int {
-        return data.size
-    }
-
-    // code for click listener
-    private var onClickListener: OnClickListener? = null
-
-    fun setOnClickListener (onClickListener: OnClickListener){
-        this.onClickListener = onClickListener
-    }
-
-    interface OnClickListener {
-        fun onClick (position: Int, model: ModelMovie)
+    override fun getItemCount(): Int = data.size
+    
+    
+    // add interface
+    interface OnItemClickCallback {
+        fun onItemClicked(data: Hero)
     }
 }
